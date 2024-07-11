694061d44604f381d4d16aa82004f10b5af09adb
Rebecca Hatch <rhatch88@gmail.com>
2024-07-10 20:54:33 -0400

    7/10 update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const initSlider = () => {
-    const slideButtons = document.querySelectorAll(".slider-wrapper slide-button")
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button")
 }
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
-            console.log(button);
+            const direction = button.id === ""
         })
     })
 
