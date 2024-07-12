e3372f969883328f6d5e99bc3382a87c0b1d61b6
Rebecca Hatch <rhatch88@gmail.com>
2024-07-12 08:41:03 -0400

    adding about us backgroung options

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const initSlider = () => {
-    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button")
-}
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelectorAll(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelectorAll(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    //slide images to the slide button clicks
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
-            const direction = button.id === ""
-        })
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy ({ left: scrollAmount, behavior: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    //update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = '${thumbPosition}px';
+    
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
     })
+}
 
 window.addEventListener("load", initSlider)
