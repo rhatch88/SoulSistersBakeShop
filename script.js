7714c170c0d9fbbc54de852b20ece0910aac2049
Rebecca Hatch <rhatch88@gmail.com>
2024-07-15 13:03:33 -0400

    Last push before trying to add 2C grid w/ google maps API

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const initSlider = () => {
     const imageList = document.querySelector(".slider-wrapper .image-list");
     const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-    const sliderScrollbar = document.querySelectorAll(".container .slider-scrollbar");
-    const scrollbarThumb = sliderScrollbar.querySelectorAll(".scrollbar-thumb");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
     const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    //Handles scrollbar thumb drag
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        // Updates thumb position on mouse move
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+            
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${newThumbPosition}px`;
+            imageList.scrollLeft = scrollPosition;
+        }
+
+        //remove event listeners on mouse up
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove)
+            document.removeEventListener("mouseup", handleMouseUp)
+        }
+
+        document.addEventListener("mousemove", handleMouseMove)
+        document.addEventListener("mouseup", handleMouseUp)
+    })
 
     //slide images to the slide button clicks
     slideButtons.forEach(button => {
@@ -23,8 +51,7 @@ const initSlider = () => {
     const updateScrollThumbPosition = () => {
         const scrollPosition = imageList.scrollLeft;
         const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
-        scrollbarThumb.style.left = '${thumbPosition}px';
-    
+        scrollbarThumb.style.left = `${thumbPosition}px`;
     }
 
     imageList.addEventListener("scroll", () => {
